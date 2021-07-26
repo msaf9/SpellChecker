c6a3b508ce8f37cb7dd487b25acba7739cdddcfa
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-07-26 23:53:51 +0530

    code changes

--- SpellCheckerProject.C
+++ SpellCheckerProject.C
@@ -5,60 +5,63 @@
    Concepts used: Files, loops, control statements, and variable declaration
    File name: Imp.txt
  */
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-#include<process.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <process.h>
+#include <stdlib.h>
 void main()
 {
-    int i=0,a,flag=0;
-    char w[20],x[20],c;
+    int i = 0, a, flag = 0;
+    char w[20], x[20], c;
     FILE *dict;
-    clrscr();
+    system("clear");
     printf("HELLO\nWELCOME TO SPELL CHECKER");
     printf("\nEnter word:");
     gets(w);
-    dict=fopen("Imp.txt","r");
-    if(dict==NULL)
+    dict = fopen("Imp.txt", "r");
+    if (dict == NULL)
     {
-        printf("\nUnable to process ;(\nError code #707\nPlease reach out developer :)");
+        printf("\nUnable to process ;(\nError code #707\nYour dictionary file is missing!\nPlease reach out developer :(");
         getch();
         exit(0);
     }
     else
     {
-        i=0;
-        while(((c=getc(dict))!=EOF))
+        i = 0;
+        while (((c = getc(dict)) != EOF))
         {
-            if((c==' '||c=='\n'||c=='\t')&&(flag==0))
+            if ((c == ' ' || c == '\n' || c == '\t') && (flag == 0))
             {
-                x[i]='\0';
-                a=strcmpi(w,x);
-                if(a==0)
+                x[i] = '\0';
+                a = strcmpi(w, x);
+                if (a == 0)
                 {
-                    flag=1;
+                    flag = 1;
                     break;
                 }
                 else
                 {
-                    flag=0;
+                    flag = 0;
                 }
-                i=0;
+                i = 0;
             }
             else
             {
-                x[i]=c;
+                x[i] = c;
                 i++;
             }
         }
     }
-    if(flag==1)
+    if (flag == 1)
     {
         printf("\nCORRECT SPELLING :)\nTHANK YOU\nHAVE A NICE DAY :)");
+        exit(0);
     }
     else
     {
         printf("\nINCORRECT SPELLING :(\nTRY AGIAN......!");
+        exit(0);
     }
     getch();
 }
